16dc8a5513f37741f23e3d8fc5715b9c1923bfe0
FacuConde <128649179+FacuConde@users.noreply.github.com>
2025-04-24 13:17:54 -0300

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="footer-contenido">
+      <p>&copy; 2025 Imperium Restaurant - Alta cocina en sombras</p>
+      <p>Síguenos en:
+        <a href="https://www.facebook.com/imperiumrestaurante.ec/?locale=es_LA" className="red-social" target="_blank" rel="noopener noreferrer">Facebook</a> |
+        <a href="https://www.instagram.com/imperiumrestaurante.ec/?hl=es" className="red-social" target="_blank" rel="noopener noreferrer">Instagram</a> |
+        <a href="https://x.com/RestImperium" className="red-social" target="_blank" rel="noopener noreferrer">Twitter ("x")</a>
+      </p>
+    </div>
+  </footer>
+);
+
+export default Footer;
